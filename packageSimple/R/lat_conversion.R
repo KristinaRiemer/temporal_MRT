db7b8ac5b6825fdfee9252d1d8d9c72782741a8d
Kristina Riemer <kristina.riemer@weecology.org>
2018-09-21 12:14:58 -0400

    Add documentation for functions with Roxygen2

--- packageSimple/R/lat_conversion.R
+++ packageSimple/R/lat_conversion.R
@@ -1,3 +1,16 @@
+#' Latitudinal coordinate conversion
+#' 
+#' Converts a given latitude into the temperature grid format. 
+#' 
+#' @param coord_lat numerical latitude to input
+#' 
+#' @return numerical latitude in grid format
+#' 
+#' @examples 
+#' lat_conversion(31.93774)
+#' 
+#' @export
+#' 
 lat_conversion = function(coord_lat){
   coord_lat_double = coord_lat * 2
   grid_lat = 180 - coord_lat_double
--- packageSimple/R/lon_conversion.R
+++ packageSimple/R/lon_conversion.R
@@ -1,3 +1,16 @@
+#' Longitudinal coordinate conversion
+#' 
+#' Converts a given longitude into the temperature grid format. 
+#' 
+#' @param coord_lon numerical longitude to input
+#' 
+#' @return numerical longitude in grid format
+#' 
+#' @examples 
+#' lon_conversion(-109.08013)
+#' 
+#' @export
+#' 
 lon_conversion = function(coord_lon){
   coord_lon_double = coord_lon * 2
   if(coord_lon_double < 0){
