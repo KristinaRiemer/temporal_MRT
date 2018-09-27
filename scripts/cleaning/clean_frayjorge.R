50e6f67ba0cff4687290787729d5492bcb3b17d7
Kristina Riemer <kristina.riemer@weecology.org>
2018-09-27 15:47:38 -0400

    Add documentation and reformat data cleaning scripts

--- scripts/cleaning/clean_frayjorge.R
+++ scripts/cleaning/clean_frayjorge.R
@@ -1,21 +1,30 @@
+library(rdataretriever)
+library(tidyr)
+library(dplyr)
 
-
-if(!file.exists("../data/raw_frayjorge/fray_jorge_ecology_mammals.csv")){
+# Download and read in occurrences data
+occurrences_path = "data/frayjorge/raw/fray_jorge_ecology_mammals.csv"
+if(!file.exists(occurrences_path)){
   rdataretriever::install(dataset = "fray-jorge-ecology", connection = "csv", data_dir = "../data/raw_frayjorge/")
 }
+occurrences = read.csv(occurrences_path, na.strings = ".")
 
-fray_occurrences = read.csv("../data/raw_frayjorge/fray_jorge_ecology_mammals.csv", na.strings = ".")
-dim(fray_occurrences)
-head(fray_occurrences)
-
-fray_occurrences = fray_occurrences %>% 
+# Split date column into month and year columns
+# TODO: add test for correct year and month ranges
+occurrences = occurrences %>% 
   separate(mo, into = c("year", "month"), sep = 4, remove = FALSE, convert = TRUE)
 
-by_sp_yr_fray_jorge = fray_occurrences %>% 
-  filter(!is.na(wt), !is.na(sp)) %>% 
+# Remove occurrences and get annual species masses
+# TODO: add test to make sure there are no NA values in weight or species columns
+clean_occurrences = occurrences %>% 
+  filter(!is.na(wt), 
+         !is.na(sp)) %>% 
   group_by(sp, year) %>% 
-  summarize(mass_mean = mean(wt), 
+  summarise(mass_mean = mean(wt), 
             mass_sd = sd(wt)) %>% 
-  mutate(species = sp, 
+  rename(species = sp, 
          yr = year)
-by_sp_yr_fray_jorge
+  
+# Save cleaned occurrences data
+clean_occurrences_path = "data/frayjorge/clean/occurrences.csv"
+write.csv(clean_occurrences, clean_occurrences_path)
--- scripts/cleaning/clean_portal.R
+++ scripts/cleaning/clean_portal.R
@@ -1,29 +1,29 @@
+library(rdataretriever)
 library(dplyr)
 
-if(!file.exists("../data/raw_portal/portal_dev_rodent_trapping.csv")){
+# Download and read in occurrences data
+occurrences_path = "data/portal/raw/portal_dev_rodent.csv"
+if(!file.exists(occurrences_path)){
   rdataretriever::install(dataset = "portal-dev", connection = "csv", data_dir = "../data/raw_portal/")
 }
+occurrences = read.csv(occurrences_path)
 
-portal_occurrences = read.csv("../data/raw_portal/portal_dev_rodent.csv")
-dim(portal_occurrences)
-head(portal_occurrences)
-
-colnames(portal_occurrences)
-unique(portal_occurrences$age)
-head(unique(portal_occurrences$tag), n = 20)
-head(unique(portal_occurrences$ltag), n = 20)
-
-portal_species = read.csv("../data/raw_portal/portal_dev_rodent_species.csv")
-portal_species
-
-table(portal_species$rodent)
-rodent_list = portal_species %>% 
+# List of rodent species
+species_path = "data/portal/raw/portal_dev_rodent_species.csv"
+species = read.csv(species_path)
+species_rodents = species %>% 
   filter(rodent == 1)
-rodent_list
 
-by_sp_yr_portal = portal_occurrences %>% 
-  filter(!is.na(wgt), !is.na(species), species %in% rodent_list$species_code) %>% 
+# Remove occurrences and get annual species masses
+# TODO: add test to make sure there are no NA values in weight or species columns
+clean_occurrences = occurrences %>% 
+  filter(!is.na(wgt), 
+         !is.na(species), 
+         species %in% species_rodents$species_code) %>% 
   group_by(species, yr) %>% 
-  summarize(mass_mean = mean(wgt), 
+  summarise(mass_mean = mean(wgt), 
             mass_sd = sd(wgt))
-by_sp_yr_portal
+
+# Save cleaned occurrences data
+clean_occurrences_path = "data/portal/clean/occurrences.csv"
+write.csv(clean_occurrences, clean_occurrences_path)
