858416feb67986dc1b3ca34a709ea0c6c8a53222
Kristina Riemer <kristina.riemer@weecology.org>
2018-09-28 10:52:54 -0400

    Add documentation and reformat script for getting all site annual temps

--- scripts/cleaning/site_temps.R
+++ scripts/cleaning/site_temps.R
@@ -1,9 +1,9 @@
+library(ncdf4)
 library(dplyr)
+library(ggplot2)
 
-# TODO: read in site locations data
-
-#### Convert coordinates to grid format ####
-
+# Functions
+# TODO: move conversion functions into their own scripts as part of package
 lon_conversion = function(coord_lon){
   coord_lon_double = coord_lon * 2
   if(coord_lon_double < 0){
@@ -20,45 +20,52 @@ lat_conversion = function(coord_lat){
   return(grid_lat)
 }
 
+# Read in site locations data
+site_locations = read.csv("data/site_locations.csv")
+
+# Download and read in all temperature data
+temperature_path = "data/temperature/air.mon.mean.v401.nc"
+if(!file.exists(temperature_path)){
+  download.file(url = "ftp://ftp.cdc.noaa.gov/Datasets/udel.airt.precip/air.mon.mean.v401.nc", 
+                destfile = temperature_path)
+}
+
+temperature_nc = nc_open(temperature_path)
+temperature_df = ncvar_get(nc = temperature_nc, 
+                              varid = attributes(temperature_nc$var)$names)
+
+# Convert coordinates to grid format
 site_locations = site_locations %>% 
   rowwise() %>% 
   mutate(grid_lon = lon_conversion(lon), 
          grid_lat = lat_conversion(lat))
-site_locations
 
-# TODO: remove this temporary fix
+# TODO: remove this temporary fix once Fray Jorge location is more accurate
 site_locations$grid_lon[2] = site_locations$grid_lon[2] + 2
-site_locations
 
-#### Plot all site locations on one month of temp data ####
-
-if(!file.exists("../data/temperature/air.mon.mean.v401.nc")){
-  download.file("ftp://ftp.cdc.noaa.gov/Datasets/udel.airt.precip/air.mon.mean.v401.nc", "../data/temperature/air.mon.mean.v401.nc")
-}
-
-temperature_df = nc_open("../data/temperature/air.mon.mean.v401.nc")
-
-temp_variable_all = ncvar_get(temperature_df, attributes(temperature_df$var)$names)
-
-if(!file.exists("../data/temperature/single_year_temp.csv")){
-  single_year_temp = expand.grid(1:359, 1:719)
-  names(single_year_temp) = c("lat", "lon")
-  single_year_temp$temp = NA
-  for(i in 1:nrow(single_year_temp)){
-    single_year_temp[i, "temp"] = temp_variable_all[single_year_temp$lon[i], single_year_temp$lat[i], 1380]
+# Create, save, and read in csv for a single month of temperature data
+single_month_path = "data/temperature/single_month.csv"
+if(!file.exists(single_month_path)){
+  single_month_temp = expand.grid(1:359, 1:719)
+  names(single_month_temp) = c("lat", "lon")
+  single_month_temp$temp = NA
+  for(i in 1:nrow(single_month_temp)){
+    single_month_temp[i, "temp"] = temperature_df[single_month_temp$lon[i], 
+                                                  single_month_temp$lat[i], 
+                                                  1380]
   }
-  write.csv(single_year_temp, "../data/temperature/single_year_temp.csv")
+  print(single_month_temp)
+  write.csv(single_month_temp, single_month_path)
 }
+single_month_temp = read.csv(single_month_path)
 
-single_year_temp = read.csv("../data/temperature/single_year_temp.csv")
-
-ggplot(single_year_temp, aes(lon, lat)) +
+# Plot site locations on top of single month of temperature
+ggplot(single_month_temp, aes(lon, lat)) +
   geom_tile(aes(fill = temp)) +
   geom_point(data = site_locations, aes(x = grid_lon, y = grid_lat)) +
   scale_y_reverse()
 
-#### Get mean annual temp for each year for all sites ####
-
+# Create temperature month index lookup table
 months = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
 years = c()
 for(year in 1900:2014){
@@ -67,27 +74,27 @@ for(year in 1900:2014){
 }
 indices = data.frame(index = seq(1, 1380), month = rep(months, 115), year = years)
 indices$month = factor(indices$month, levels = month.abb)
-indices
 
+# Get all monthly temperatures for each site
 monthly_temps = data.frame(site = character(), month = numeric(), temp = numeric())
 for(site in site_locations$site_name){
   lon = site_locations$grid_lon[site_locations$site_name == site]
   lat = site_locations$grid_lat[site_locations$site_name == site]
   for(index in indices$index){
-    temp = temp_variable_all[lon, lat, index]
+    temp = temperature_df[lon, lat, index]
     each_temp = data.frame(site = site, index = index, temp = temp)
     monthly_temps = rbind(monthly_temps, each_temp)
   }
 }
-monthly_temps
-
 monthly_temps = monthly_temps %>% 
   left_join(indices, by = "index")
-monthly_temps
 
+# Get all annual temperatures for each site
 annual_temps = monthly_temps %>%
   group_by(site, year) %>% 
   summarise(avg_temp = mean(temp), 
             avg_temp_sd = sd(temp))
-annual_temps
 
+# Save site annual temperatures
+annual_temps_path = "data/site_annual_temps.csv"
+write.csv(annual_temps, annual_temps_path)
