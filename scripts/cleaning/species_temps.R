0da1a98bd51dd2904efb2d89b82023486057e910
Kristina Riemer <kristina.riemer@weecology.org>
2018-09-28 12:10:05 -0400

    Add documentation and reformat script for combining occurrence and temp data

--- scripts/cleaning/species_temps.R
+++ scripts/cleaning/species_temps.R
@@ -1,24 +1,34 @@
 library(dplyr)
 
-# TODO: read in temperature and mass dataframes
+# TODO: combine all site occurrences in separate script
+# TODO: simplify this script
 
+# Read in temperature and occurrence data
+site_annual_temps = read.csv("data/site_annual_temps.csv")
+occurrences_portal = read.csv("data/portal/clean/occurrences.csv")
+occurrences_fray_jorge = read.csv("data/frayjorge/clean/occurrences.csv")
+
+# Get annual temperature for each species
 sites = c("portal", "fray_jorge")
-all_annual_masses = data.frame(species = factor(), yr = numeric(), mass_mean = numeric(), avg_temp = numeric(), site = factor())
+occurrences_with_temp = data.frame(species = factor(), yr = numeric(), mass_mean = numeric(), avg_temp = numeric(), site = factor())
 for(each_site in sites){
-  site_annual_temps = annual_temps %>% 
+  annual_temps_by_site = site_annual_temps %>% 
     filter(site == each_site) %>% 
     data.frame() %>% 
     select(year, avg_temp)
-  by_sp_yr_name = eval(parse(text = paste("by_sp_yr", each_site, sep = "_")))
-  site_annual_masses = by_sp_yr_name %>%
-    group_by(species) %>% 
-    add_tally() %>% 
+  occurrences_by_site = eval(parse(text = paste("occurrences", each_site, sep = "_")))
+  occurrences_by_site = occurrences_by_site %>%
+    group_by(species) %>%
+    add_tally() %>%
     filter(n > 5) %>%
-    ungroup() %>% 
+    ungroup() %>%
     select(species, yr, mass_mean)
-  site_annual_masses = site_annual_masses %>%
-    left_join(site_annual_temps, by = c("yr" = "year"))
-  site_annual_masses$site = as.factor(each_site)
-  all_annual_masses = rbind(all_annual_masses, site_annual_masses)
+  occurrences_with_temp_by_site = occurrences_by_site %>%
+    left_join(annual_temps_by_site, by = c("yr" = "year"))
+  occurrences_with_temp_by_site$site = as.factor(each_site)
+  occurrences_with_temp = rbind(occurrences_with_temp, occurrences_with_temp_by_site)
 }
-all_annual_masses
+
+# Save combined occurrences/temperature data
+occurrences_with_temp_path = "data/occurrences_with_temp.csv"
+write.csv(occurrences_with_temp, occurrences_with_temp_path)
