8070f2114bf06e8a9ab7918b4928b7059820e8af
Kristina Riemer <kristina.riemer@weecology.org>
2018-09-28 15:26:48 -0400

    Add documentation and reformat script for producing 3-panel fig

--- scripts/analysis/main_fig.R
+++ scripts/analysis/main_fig.R
@@ -1,8 +1,16 @@
+library(dplyr)
+library(tidyr)
 library(purrr)
 library(ggplot2)
 library(cowplot)
 
-plots = all_annual_masses %>% 
+# Read in occurrences with temperature data
+occurrences_with_temp_path = "data/occurrences_with_temp.csv"
+occurrences_with_temp = read.csv(occurrences_with_temp_path)
+
+# TODO: double check that range of temperature years match occurrences years
+# Generate three-panel figure for each site
+plots_df = occurrences_with_temp %>% 
   group_by(site) %>% 
   nest() %>% 
   mutate(
@@ -13,19 +21,16 @@ plots = all_annual_masses %>%
                                geom_point() +
                                stat_smooth(method = "lm") +
                                facet_wrap(~species, scales = "free", ncol = 1)),
-    mrt = purrr::map(data, ~ ggplot(., aes(avg_temp, mass_mean)) + 
+    mrt = purrr::map(data, ~ ggplot(., aes(avg_temp, mass_mean)) +
                        geom_point() +
                        stat_smooth(method = "lm") +
-                       facet_wrap(~species, scales = "free", ncol = 1))
+                       facet_wrap(~species, scales = "free", ncol = 1)),
+    combined = purrr::pmap(list(temp_yearly, mass_yearly, mrt), ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2, ..3, nrow = 1), ncol = 1))
   )
-plots
 
-plots$combined_plots = purrr::pmap(plots, ~cowplot::plot_grid(..4, ..5, align = "h"))
-plots$combined_plots
-plots$more_combined_plots = purrr::pmap(plots, ~cowplot::plot_grid(..3, ..6, nrow = 2, align = "v"))
-plots$more_combined_plots
-
-plots$combined = plots %>% 
-  purrr::pmap(., ~cowplot::plot_grid(..4, ..5, align = "h")) %>% 
-  purrr::pmap(., ~cowplot::plot_grid(..3, ..6, nrow = 2, align = "v"))
-plots
+# TODO: automatically determine height of plot from number of species
+# Save final figure for each site
+sites = unique(occurrences_with_temp$site)
+site_file_names = paste0("plots/", sites, "/", sites, ".png")
+site_file_names
+map2(site_file_names, plots_df$combined, ggsave)
