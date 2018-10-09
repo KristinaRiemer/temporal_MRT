4395dcc530251c01487b2659346bc2759429be16
Kristina Riemer <kristina.riemer@weecology.org>
2018-10-09 17:28:37 -0500

    Update size distribution axis labels

--- temporal_MRT/R/plot_size_distributions.R
+++ temporal_MRT/R/plot_size_distributions.R
@@ -2,10 +2,12 @@ plot_size_distributions = function(site_data){
   size_hist = ggplot(site_data, aes(x = mass_mean)) +
     geom_histogram() +
     scale_x_log10() +
+    labs(x = "Mean species size", y = "Number of species") +
     facet_wrap(~yr)
   size_density = ggplot(site_data, aes(x = mass_mean)) +
     geom_density() +
     scale_x_log10() +
+    labs(x = "Mean species size", y = "Density of species") +
     facet_wrap(~yr)
   size_both = plot_grid(size_hist, size_density)
   return(size_both)
