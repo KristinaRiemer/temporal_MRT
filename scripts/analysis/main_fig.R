3610c899cb6f5a0887fe819bebf870fa8622060d
Kristina Riemer <kristina.riemer@weecology.org>
2018-10-18 13:37:58 -0400

    Start fixing dimensions of main plot

--- scripts/analysis/main_fig.R
+++ scripts/analysis/main_fig.R
@@ -25,11 +25,30 @@ plots_df = occurrences_with_temp %>%
                        geom_point() +
                        stat_smooth(method = "lm") +
                        facet_wrap(~species, scales = "free", ncol = 1)),
-    combined = purrr::pmap(list(temp_yearly, mass_yearly, mrt), ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2, ..3, nrow = 1), ncol = 1))
+    # combined = purrr::pmap(list(temp_yearly, mass_yearly, mrt), 
+    #                        ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2, ..3, nrow = 1, rel_widths = c(.25, .25)), 
+    #                                             ncol = 1, rel_heights = c(1, 5), rel_widths = c(1, 5))), 
+    combined2 = purrr::pmap(list(temp_yearly, mass_yearly, mrt), 
+                           ~ ggdraw(plot = NULL, xlim = c(0, 1), ylim = c(0, 10)) + 
+                             draw_plot(..1, 0, 7.5, width = 1, height = 1) + 
+                             draw_plot(..2, 0, 0, width = 0.5, height = 7.5) + 
+                             draw_plot(..3, 0.5, 0, width = 0.5, height = 7.5))
+    #plot_name = purrr::map(data, paste0("plots/", site))
+    #ggsave()
   )
+plots_df$combined2[2]
+
+# ggdraw() +
+#   draw_plot(plots_df$temp_yearly[2]) +
+#   draw_plot(plots_df$mass_yearly[2]) +
+#   draw_plot(plots_df$mrt[2])
 
 # TODO: automatically determine height of plot from number of species
 # Save final figure for each site
 sites = unique(occurrences_with_temp$site)
 site_file_names = paste0("plots/", sites, "/", sites, "_main.png")
-map2(site_file_names, plots_df$combined, ggsave)
+plots_df$height = c(20, 10)
+ggsave_args = list(filename = site_file_names, 
+                   plot = plots_df$combined2, 
+                   height = plots_df$height)
+pmap(ggsave_args, ggsave)
