cc0069c222c63c5e3fafec50d8e096e9cd0aac48
Kristina Riemer <kristina.riemer@weecology.org>
2018-11-05 10:35:08 -0500

    Fix across site summary calculations

--- scripts/analysis/site_summary_stats.R
+++ scripts/analysis/site_summary_stats.R
@@ -6,21 +6,21 @@ library(flextable)
 occurrences_with_temp_path = "data/occurrences_with_temp.csv"
 occurrences_with_temp = read.csv(occurrences_with_temp_path)
 
-# Create dataframe with summary values and states across sites
-across_site_summary = occurrences_with_temp %>% 
-  summarize(num_inds = sum(inds), 
-            num_species = n_distinct(scientific_name))
-
 # Create dataframe with summary values and stats for each site
 by_site_summary = occurrences_with_temp %>% 
   group_by(site) %>% 
   summarize(num_inds = sum(inds),
-            num_species = n_distinct(scientific_name), 
+            num_species = n_distinct(species), 
             num_yr = n_distinct(yr), 
             min_inds = min(inds), 
             max_inds = max(inds), 
             min_yr = min(num_yrs), 
             max_yr = max(num_yrs))
+
+# Create dataframe with summary values and states across sites
+across_site_summary = by_site_summary %>% 
+  summarize(num_inds = sum(num_inds), 
+            num_species = sum(num_species))
 
 # Save both dataframes as tables in .docx
 ft_across_site_summary = flextable(across_site_summary)
