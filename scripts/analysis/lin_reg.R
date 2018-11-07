8f5171f3de98616fe5b7a04fd1ce290c2654e5db
Kristina Riemer <kristina.riemer@weecology.org>
2018-11-07 16:40:24 -0500

    Split main figure into 2, add hist of mass by year slopes

--- scripts/analysis/lin_reg.R
+++ scripts/analysis/lin_reg.R
@@ -6,38 +6,59 @@ library(broom)
 occurrences_with_temp_path = "data/occurrences_with_temp.csv"
 occurrences_with_temp = read.csv(occurrences_with_temp_path)
 
-# Create dataframe of r and p values for all species at each site
-# TODO: break this code up into smaller chunks
+# Create dataframe of slope for each species mass over time
 sites = unique(occurrences_with_temp$site)
-all_lm = data.frame(species = factor(), r.squared = numeric(), slope = numeric(), p.value = numeric(), r = numeric(), site = factor(), scientific_name = factor())
+all_lm_mass = data.frame(species = factor(), slope = numeric(), site = factor(), scientific_name = factor())
 for(each_site in sites){
   site_annual_masses = occurrences_with_temp[occurrences_with_temp$site == each_site,]
-  site_lm_tidy = site_annual_masses %>%
+  site_lm_mass = site_annual_masses %>% 
+    nest(-species) %>% 
+    mutate(fit = purrr::map(data, ~lm(mass_mean ~ yr, data = .)), 
+           results = purrr::map(fit, tidy)) %>% 
+    unnest(results) %>% 
+    filter(term == "yr") %>% 
+    select(species, slope = estimate)
+  site_lm_mass$site = as.factor(each_site)
+  species_codes = site_annual_masses %>% 
+    select(species, scientific_name) %>% 
+    distinct(species, scientific_name)
+  site_lm_mass = left_join(site_lm_mass, species_codes, by = c("species" = "species"))
+  all_lm_mass = rbind(all_lm_mass, site_lm_mass)
+}
+
+# Create dataframe of r and p values for each species temp-mass relationship
+all_lm_mrt = data.frame(species = factor(), r.squared = numeric(), slope = numeric(), p.value = numeric(), r = numeric(), site = factor(), scientific_name = factor())
+for(each_site in sites){
+  site_annual_masses = occurrences_with_temp[occurrences_with_temp$site == each_site,]
+  site_lm_tidy_mrt = site_annual_masses %>%
     nest(-species) %>% 
     mutate(fit = purrr::map(data, ~lm(mass_mean ~ avg_temp, data = .)),
            results = purrr::map(fit, tidy)) %>%
     unnest(results) %>%
     filter(term == "avg_temp") %>%
     select(species, slope = estimate, p.value)
-  site_lm_glance = site_annual_masses %>%
+  site_lm_glance_mrt = site_annual_masses %>%
     nest(-species) %>%
     mutate(fit = purrr::map(data, ~lm(mass_mean ~ avg_temp, data = .)),
            results = purrr::map(fit, glance)) %>%
     unnest(results) %>%
     select(species, r.squared)
-  site_lm = site_lm_glance %>%
-    full_join(site_lm_tidy, by = "species")
-  site_lm = site_lm %>%
+  site_lm_mrt = site_lm_glance_mrt %>%
+    full_join(site_lm_tidy_mrt, by = "species")
+  site_lm_mrt = site_lm_mrt %>%
     mutate(r = case_when(slope > 0 ~ sqrt(r.squared),
                          slope < 0 ~ -sqrt(r.squared)))
-  site_lm$site = as.factor(each_site)
+  site_lm_mrt$site = as.factor(each_site)
   species_codes = site_annual_masses %>% 
     select(species, scientific_name) %>% 
     distinct(species, scientific_name)
-  site_lm = left_join(site_lm, species_codes, by = c("species" = "species"))
-  all_lm = rbind(all_lm, site_lm)
+  site_lm_mrt = left_join(site_lm_mrt, species_codes, by = c("species" = "species"))
+  all_lm_mrt = rbind(all_lm_mrt, site_lm_mrt)
 }
 
 # Save linear regression results dataframe
-lin_reg_stats_path = "data/lin_reg_stats.csv"
-write.csv(all_lm, lin_reg_stats_path)
+lin_reg_mass_path = "data/lin_reg_mass.csv"
+write.csv(all_lm_mass, lin_reg_mass_path)
+
+lin_reg_mrt_path = "data/lin_reg_mrt.csv"
+write.csv(all_lm_mrt, lin_reg_mrt_path)
--- scripts/analysis/main_fig.R
+++ scripts/analysis/main_fig.R
@@ -8,25 +8,31 @@ library(cowplot)
 occurrences_with_temp_path = "data/occurrences_with_temp.csv"
 occurrences_with_temp = read.csv(occurrences_with_temp_path)
 
-# Read in linear regression results data
-lin_reg_stats_path = "data/lin_reg_stats.csv"
-lin_reg_stats = read.csv(lin_reg_stats_path)
+# Read in linear regression results for both yearly mass and mass-temp relationships
+lin_reg_mass_path = "data/lin_reg_mass.csv"
+lin_reg_mass = read.csv(lin_reg_mass_path)
+lin_reg_mrt_path = "data/lin_reg_mrt.csv"
+lin_reg_mrt = read.csv(lin_reg_mrt_path)
 
 # Create four plots for each site
 occurrences_with_temp = occurrences_with_temp %>% 
-  mutate(label1 = case_when(site == "portal" ~ "A", 
+  mutate(temp_by_year_label = case_when(site == "portal" ~ "A", 
                             site == "frayjorge" ~ "B", 
                             site == "sevilleta" ~ "C"), 
-         label2 = case_when(site == "portal" ~ "D", 
+         mass_by_year_label = case_when(site == "portal" ~ "D", 
                             site == "frayjorge" ~ "E", 
                             site == "sevilleta" ~ "F"), 
-         label3 = case_when(site == "portal" ~ "G", 
+         mass_by_temp_label = case_when(site == "portal" ~ "A", 
+                            site == "frayjorge" ~ "B", 
+                            site == "sevilleta" ~ "C"))
+lin_reg_mrt = lin_reg_mrt %>% 
+  mutate(r_dist_label = case_when(site == "portal" ~ "D", 
+                            site == "frayjorge" ~ "E", 
+                            site == "sevilleta" ~ "F"))
+lin_reg_mass = lin_reg_mass %>% 
+  mutate(slope_dist_label = case_when(site == "portal" ~ "G", 
                             site == "frayjorge" ~ "H", 
                             site == "sevilleta" ~ "I"))
-lin_reg_stats = lin_reg_stats %>% 
-  mutate(label4 = case_when(site == "portal" ~ "J", 
-                            site == "frayjorge" ~ "K", 
-                            site == "sevilleta" ~ "L"))
 
 three_plots_df = occurrences_with_temp %>% 
   group_by(site) %>% 
@@ -36,52 +42,85 @@ three_plots_df = occurrences_with_temp %>%
                                 geom_point() +
                                 stat_smooth(method = "lm") +
                                 labs(x = "Year", y = "Mean Temp (C*)") +
-                                geom_text(aes(x = min(yr), y = max(avg_temp), label = label1), 
+                                geom_text(aes(x = min(yr), y = max(avg_temp), label = temp_by_year_label), 
                                           fontface = "bold")), 
     mass_by_year = purrr::map(data, ~ ggplot(., aes(yr, mass_mean, color = species)) +
                                 geom_point() +
                                 stat_smooth(method = "lm", se = FALSE) +
                                 theme(legend.position = "none") +
                                 labs(x = "Year", y = "Mean Mass (g)") +
-                                geom_text(aes(x = min(yr), y = max(mass_mean), label = label2), 
+                                geom_text(aes(x = min(yr), y = max(mass_mean), label = mass_by_year_label), 
                                           fontface = "bold", color = "black")),
     mass_by_temp = purrr::map(data, ~ ggplot(., aes(avg_temp, mass_mean, color = species)) +
                                 geom_point() +
                                 stat_smooth(method = "lm", se = FALSE) +
                                 theme(legend.position = "bottom") +
                                 labs(x = "Mean Temp (C*)", y = "Mean Mass (g)") +
-                                geom_text(aes(x = min(avg_temp), y = max(mass_mean), label = label3), 
+                                geom_text(aes(x = min(avg_temp), y = max(mass_mean), label = mass_by_temp_label), 
                                           fontface = "bold", color = "black"))
   )
 
-last_plot_df = lin_reg_stats %>% 
+mrt_plot_df = lin_reg_mrt %>% 
   group_by(site) %>% 
   nest() %>% 
   mutate(r_dist = purrr::map(data, ~ggplot(., aes(x = r)) +
                                geom_histogram() +
                                xlim(c(-1, 1)) +
+                               geom_vline(xintercept = 0, color = "red") +
                                labs(x = "R", y = "Number of species") +
-                               geom_text(aes(x = -1, y = 2, label = label4), 
+                               geom_text(aes(x = -1, y = 2, label = r_dist_label), 
                                          fontface = "bold"))
+  )
+
+mass_plot_df = lin_reg_mass %>% 
+  group_by(site) %>% 
+  nest() %>% 
+  mutate(slope_dist = purrr::map(data, ~ggplot(., aes(x = slope)) +
+                                   geom_histogram() +
+                                   xlim(c(-1, 1)) +
+                                   geom_vline(xintercept = 0, color = "red") +
+                                   labs(x = "Yearly mass slope", y = "Number of species") +
+                                   geom_text(aes(x = -1, y = 3, label = slope_dist_label), 
+                                             fontface = "bold"))
   )
 
 # Combine four plots for each site, then combine all site plots
 # TODO: create the 12-panel plot in a better way
-plots_df = left_join(three_plots_df, last_plot_df, by = "site")
-plots_df$combined = purrr::pmap(list(plots_df$temp_by_year, plots_df$mass_by_year, 
-                                     plots_df$mass_by_temp, plots_df$r_dist), 
+plots_df = left_join(three_plots_df, mrt_plot_df, by = "site")
+plots_df = left_join(plots_df, mass_plot_df, by = "site")
+
+plots_df$combined1 = purrr::pmap(list(plots_df$temp_by_year, plots_df$mass_by_year, 
+                                     plots_df$slope_dist), 
                                 ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2), 
-                                                     plot_grid(..3), plot_grid(..4), 
+                                                     plot_grid(..3), 
                                                      ncol = 1))
 
-combined_sites = purrr::pmap(list(plots_df$combined[1], plots_df$combined[2], 
-                                  plots_df$combined[3]), 
+plots_df$combined2 = purrr::pmap(list(plots_df$mass_by_temp, plots_df$r_dist), 
+                                ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2), 
+                                                     ncol = 1))
+
+
+
+
+combined_sites1 = purrr::pmap(list(plots_df$combined1[1], plots_df$combined1[2], 
+                                  plots_df$combined1[3]), 
                              ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2),
                                                   plot_grid(..3), nrow = 1))
 
-# Save figure
-ggsave_args = list(filename = "plots/main_fig.png", 
-                   plot = combined_sites, 
-                   width = 15, 
-                   height = 20)
-pmap(ggsave_args, ggsave)
+combined_sites2 = purrr::pmap(list(plots_df$combined2[1], plots_df$combined2[2], 
+                                   plots_df$combined2[3]), 
+                              ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2),
+                                                   plot_grid(..3), nrow = 1))
+
+# Save figures
+ggsave_args1 = list(filename = "plots/main_fig1.png", 
+                   plot = combined_sites1, 
+                   width = 14, 
+                   height = 12)
+pmap(ggsave_args1, ggsave)
+
+ggsave_args2 = list(filename = "plots/main_fig2.png", 
+                    plot = combined_sites2, 
+                    width = 16, 
+                    height = 10)
+pmap(ggsave_args2, ggsave)
