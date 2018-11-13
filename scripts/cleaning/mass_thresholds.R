f1d61947f136d5ed5e4ab0543d8088214aff1315
Kristina Riemer <kristina.riemer@weecology.org>
2018-11-13 12:31:33 -0500

    Create better sensitivity analysis fig

--- scripts/cleaning/mass_thresholds.R
+++ scripts/cleaning/mass_thresholds.R
@@ -34,17 +34,17 @@ occurrences_with_temp_actual = occurrences_with_temp %>%
   ungroup() %>% 
   filter(num_yrs >= 5)
 
-occurrences_with_temp_lower = occurrences_with_temp %>% 
+occurrences_with_temp_moreinds = occurrences_with_temp %>% 
   filter(!is.na(avg_temp)) %>% 
-  filter(inds >= 5) %>% 
+  filter(inds >= 30) %>% 
   group_by(site, species) %>% 
   mutate(num_yrs = n_distinct(yr)) %>% 
   ungroup() %>% 
-  filter(num_yrs >= 2)
+  filter(num_yrs >= 5)
 
-occurrences_with_temp_higher = occurrences_with_temp %>% 
+occurrences_with_temp_moreyrs = occurrences_with_temp %>% 
   filter(!is.na(avg_temp)) %>% 
-  filter(inds >= 30) %>% 
+  filter(inds >= 15) %>% 
   group_by(site, species) %>% 
   mutate(num_yrs = n_distinct(yr)) %>% 
   ungroup() %>% 
@@ -71,9 +71,9 @@ for(each_site in sites){
   all_lm_mass_actual = rbind(all_lm_mass_actual, site_lm_mass)
 }
 
-all_lm_mass_lower = data.frame(species = factor(), slope = numeric(), site = factor(), scientific_name = factor())
+all_lm_mass_moreinds = data.frame(species = factor(), slope = numeric(), site = factor(), scientific_name = factor())
 for(each_site in sites){
-  site_annual_masses = occurrences_with_temp_lower[occurrences_with_temp_lower$site == each_site,]
+  site_annual_masses = occurrences_with_temp_moreinds[occurrences_with_temp_moreinds$site == each_site,]
   site_lm_mass = site_annual_masses %>% 
     nest(-species) %>% 
     mutate(fit = purrr::map(data, ~lm(mass_mean ~ yr, data = .)), 
@@ -86,12 +86,12 @@ for(each_site in sites){
     select(species, scientific_name) %>% 
     distinct(species, scientific_name)
   site_lm_mass = left_join(site_lm_mass, species_codes, by = c("species" = "species"))
-  all_lm_mass_lower = rbind(all_lm_mass_lower, site_lm_mass)
+  all_lm_mass_moreinds = rbind(all_lm_mass_moreinds, site_lm_mass)
 }
 
-all_lm_mass_higher = data.frame(species = factor(), slope = numeric(), site = factor(), scientific_name = factor())
+all_lm_mass_moreyrs = data.frame(species = factor(), slope = numeric(), site = factor(), scientific_name = factor())
 for(each_site in sites){
-  site_annual_masses = occurrences_with_temp_higher[occurrences_with_temp_higher$site == each_site,]
+  site_annual_masses = occurrences_with_temp_moreyrs[occurrences_with_temp_moreyrs$site == each_site,]
   site_lm_mass = site_annual_masses %>% 
     nest(-species) %>% 
     mutate(fit = purrr::map(data, ~lm(mass_mean ~ yr, data = .)), 
@@ -104,59 +104,26 @@ for(each_site in sites){
     select(species, scientific_name) %>% 
     distinct(species, scientific_name)
   site_lm_mass = left_join(site_lm_mass, species_codes, by = c("species" = "species"))
-  all_lm_mass_higher = rbind(all_lm_mass_higher, site_lm_mass)
+  all_lm_mass_moreyrs = rbind(all_lm_mass_moreyrs, site_lm_mass)
 }
 
-# Create slope distribution plot for all threshold datasets for each site
-mass_plot_df_actual = all_lm_mass_actual %>% 
-  group_by(site) %>% 
-  nest() %>% 
-  mutate(slope_dist_actual = purrr::map(data, ~ggplot(., aes(x = slope)) +
-                                   geom_histogram() +
-                                   xlim(c(-1, 1)) +
-                                   geom_vline(xintercept = 0, color = "red") +
-                                   labs(x = "Yearly mass slope", y = "Number of species"))
-  )
+all_lm_mass_actual$threshold = c("Actual")
+all_lm_mass_moreinds$threshold = c("More individuals")
+all_lm_mass_moreyrs$threshold = c("More years")
+all_lm_mass = bind_rows(all_lm_mass_actual, all_lm_mass_moreinds, all_lm_mass_moreyrs)
 
-mass_plot_df_lower = all_lm_mass_lower %>% 
-  group_by(site) %>% 
-  nest() %>% 
-  mutate(slope_dist_lower = purrr::map(data, ~ggplot(., aes(x = slope)) +
-                                          geom_histogram() +
-                                          xlim(c(-1, 1)) +
-                                          geom_vline(xintercept = 0, color = "red") +
-                                          labs(x = "Yearly mass slope", y = "Number of species"))
-  )
+all_lm_mass = all_lm_mass %>% 
+  mutate(site_fancy = case_when(site == "portal" ~ "Portal", 
+                                site == "frayjorge" ~ "Fray Jorge", 
+                                site == "sevilleta" ~ "Sevilleta"))
 
-mass_plot_df_higher = all_lm_mass_higher %>% 
-  group_by(site) %>% 
-  nest() %>% 
-  mutate(slope_dist_higher = purrr::map(data, ~ggplot(., aes(x = slope)) +
-                                          geom_histogram() +
-                                          xlim(c(-1, 1)) +
-                                          geom_vline(xintercept = 0, color = "red") +
-                                          labs(x = "Yearly mass slope", y = "Number of species"))
-  )
-
-mass_plot_df = left_join(mass_plot_df_actual, mass_plot_df_lower, by = c("site" = "site"))
-mass_plot_df = left_join(mass_plot_df, mass_plot_df_higher, by = c("site" = "site"))
-
-mass_plot_df$combined = purrr::pmap(list(mass_plot_df$slope_dist_actual, mass_plot_df$slope_dist_lower, 
-                                         mass_plot_df$slope_dist_higher), 
-                                    ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2), 
-                                                         plot_grid(..3), 
-                                                         ncol = 1))
-
-combined_sites_mass = purrr::pmap(list(mass_plot_df$combined[1], mass_plot_df$combined[2], 
-                                      mass_plot_df$combined[3]), 
-                                 ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2), 
-                                                      plot_grid(..3), nrow = 1))
-
-ggsave_args_mass = list(filename = "plots/supp_mass_thresholds_1.png", 
-                    plot = combined_sites_mass, 
-                    width = 14, 
-                    height = 12)
-pmap(ggsave_args_mass, ggsave)
+# Plot slope values for three thresholds at all sites
+slopes_plot = ggplot(all_lm_mass, aes(x = threshold, y = slope)) +
+  geom_boxplot(color = "grey", outlier.shape = NA) +
+  geom_jitter(width = 0.1) +
+  ylim(c(-2, 2)) +
+  labs(x = "Threshold", y = "Mass time series slope") +
+  facet_grid(~site_fancy)
 
 # Create dataframe of r values for each species mass over time for three threshold datasets
 all_lm_mrt_actual = data.frame(species = factor(), r.squared = numeric(), slope = numeric(), p.value = numeric(), r = numeric(), site = factor(), scientific_name = factor())
@@ -188,9 +155,9 @@ for(each_site in sites){
   all_lm_mrt_actual = rbind(all_lm_mrt_actual, site_lm_mrt)
 }
 
-all_lm_mrt_lower = data.frame(species = factor(), r.squared = numeric(), slope = numeric(), p.value = numeric(), r = numeric(), site = factor(), scientific_name = factor())
+all_lm_mrt_moreinds = data.frame(species = factor(), r.squared = numeric(), slope = numeric(), p.value = numeric(), r = numeric(), site = factor(), scientific_name = factor())
 for(each_site in sites){
-  site_annual_masses = occurrences_with_temp_lower[occurrences_with_temp_lower$site == each_site,]
+  site_annual_masses = occurrences_with_temp_moreinds[occurrences_with_temp_moreinds$site == each_site,]
   site_lm_tidy_mrt = site_annual_masses %>%
     nest(-species) %>% 
     mutate(fit = purrr::map(data, ~lm(mass_mean ~ avg_temp, data = .)),
@@ -214,12 +181,12 @@ for(each_site in sites){
     select(species, scientific_name) %>% 
     distinct(species, scientific_name)
   site_lm_mrt = left_join(site_lm_mrt, species_codes, by = c("species" = "species"))
-  all_lm_mrt_lower = rbind(all_lm_mrt_lower, site_lm_mrt)
+  all_lm_mrt_moreinds = rbind(all_lm_mrt_moreinds, site_lm_mrt)
 }
 
-all_lm_mrt_higher = data.frame(species = factor(), r.squared = numeric(), slope = numeric(), p.value = numeric(), r = numeric(), site = factor(), scientific_name = factor())
+all_lm_mrt_moreyrs = data.frame(species = factor(), r.squared = numeric(), slope = numeric(), p.value = numeric(), r = numeric(), site = factor(), scientific_name = factor())
 for(each_site in sites){
-  site_annual_masses = occurrences_with_temp_higher[occurrences_with_temp_higher$site == each_site,]
+  site_annual_masses = occurrences_with_temp_moreyrs[occurrences_with_temp_moreyrs$site == each_site,]
   site_lm_tidy_mrt = site_annual_masses %>%
     nest(-species) %>% 
     mutate(fit = purrr::map(data, ~lm(mass_mean ~ avg_temp, data = .)),
@@ -243,56 +210,26 @@ for(each_site in sites){
     select(species, scientific_name) %>% 
     distinct(species, scientific_name)
   site_lm_mrt = left_join(site_lm_mrt, species_codes, by = c("species" = "species"))
-  all_lm_mrt_higher = rbind(all_lm_mrt_higher, site_lm_mrt)
+  all_lm_mrt_moreyrs = rbind(all_lm_mrt_moreyrs, site_lm_mrt)
 }
 
-# Create r distribution plot for all threshold datasets for each site
-mrt_plot_df_actual = all_lm_mrt_actual %>% 
-  group_by(site) %>% 
-  nest() %>% 
-  mutate(r_dist_actual = purrr::map(data, ~ggplot(., aes(x = r)) +
-                                          geom_histogram() +
-                                          xlim(c(-1, 1)) +
-                                          geom_vline(xintercept = 0, color = "red") +
-                                          labs(x = "R", y = "Number of species"))
-  )
+all_lm_mrt_actual$threshold = c("Actual")
+all_lm_mrt_moreinds$threshold = c("More individuals")
+all_lm_mrt_moreyrs$threshold = c("More years")
+all_lm_mrt = bind_rows(all_lm_mrt_actual, all_lm_mrt_moreinds, all_lm_mrt_moreyrs)
 
-mrt_plot_df_lower = all_lm_mrt_lower %>% 
-  group_by(site) %>% 
-  nest() %>% 
-  mutate(r_dist_lower = purrr::map(data, ~ggplot(., aes(x = r)) +
-                                      geom_histogram() +
-                                      xlim(c(-1, 1)) +
-                                      geom_vline(xintercept = 0, color = "red") +
-                                      labs(x = "R", y = "Number of species"))
-  )
+all_lm_mrt = all_lm_mrt %>% 
+  mutate(site_fancy = case_when(site == "portal" ~ "Portal", 
+                                site == "frayjorge" ~ "Fray Jorge", 
+                                site == "sevilleta" ~ "Sevilleta"))
 
-mrt_plot_df_higher = all_lm_mrt_higher %>% 
-  group_by(site) %>% 
-  nest() %>% 
-  mutate(r_dist_higher = purrr::map(data, ~ggplot(., aes(x = r)) +
-                                      geom_histogram() +
-                                      xlim(c(-1, 1)) +
-                                      geom_vline(xintercept = 0, color = "red") +
-                                      labs(x = "R", y = "Number of species"))
-  )
+# Plot r values for three thresholds at all sites
+rs_plot = ggplot(all_lm_mrt, aes(x = threshold, y = r)) +
+  geom_boxplot(color = "grey", outlier.shape = NA) +
+  geom_jitter(width = 0.1) +
+  labs(x = "Threshold", y = "Mass-temp relationship R") +
+  facet_grid(~site_fancy)
 
-mrt_plot_df = left_join(mrt_plot_df_actual, mrt_plot_df_lower, by = c("site" = "site"))
-mrt_plot_df = left_join(mrt_plot_df, mrt_plot_df_higher, by = c("site" = "site"))
-
-mrt_plot_df$combined = purrr::pmap(list(mrt_plot_df$r_dist_actual, mrt_plot_df$r_dist_lower, 
-                                         mrt_plot_df$r_dist_higher), 
-                                    ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2), 
-                                                         plot_grid(..3), 
-                                                         ncol = 1))
-
-combined_sites_mrt = purrr::pmap(list(mrt_plot_df$combined[1], mrt_plot_df$combined[2], 
-                                       mrt_plot_df$combined[3]), 
-                                  ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2), 
-                                                       plot_grid(..3), nrow = 1))
-
-ggsave_args_mrt = list(filename = "plots/supp_mass_thresholds_2.png", 
-                        plot = combined_sites_mrt, 
-                        width = 14, 
-                        height = 12)
-pmap(ggsave_args_mrt, ggsave)
+# Combine and save slope and r plots
+both_plots = plot_grid(slopes_plot, rs_plot, ncol = 1, labels = c("A", "B"))
+ggsave("plots/supp_mass_thresholds.png", plot = both_plots, width = 13, height = 11)
