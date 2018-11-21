625af74e4b4011735bf2af3997cf2ff70144a59d
Kristina Riemer <kristina.riemer@weecology.org>
2018-11-21 09:55:42 -0500

    Update Sev subsite supplement histograms, combine plots into one

--- scripts/analysis/sevilleta_subsites.R
+++ scripts/analysis/sevilleta_subsites.R
@@ -5,6 +5,8 @@ library(purrr)
 library(ggplot2)
 library(cowplot)
 library(broom)
+library(forecast)
+library(lmtest)
 
 # Read in occurrences data
 # TODO: replace download with popler package method
@@ -62,27 +64,39 @@ occurrences_subsites_with_temp = occurrences_subsites_with_temp %>%
   ungroup() %>% 
   filter(num_yrs >= 5)
 
-# Create dataframe of slope for each species mass over time
-subsites = unique(occurrences_subsites_with_temp$location)
-all_lm_mass = data.frame(species = factor(), slope = numeric(), subsite = factor(), scientific_name = factor())
-for(each_subsite in subsites){
-  subsite_annual_masses = occurrences_subsites_with_temp[occurrences_subsites_with_temp$location == each_subsite,]
-  subsite_lm_mass = subsite_annual_masses %>% 
-    nest(-species) %>% 
-    mutate(fit = purrr::map(data, ~lm(mass_mean ~ yr, data = .)), 
-           results = purrr::map(fit, tidy)) %>% 
-    unnest(results) %>% 
-    filter(term == "yr") %>%
-    select(species, slope = estimate)
-  subsite_lm_mass$subsite = as.factor(each_subsite)
-  species_codes = subsite_annual_masses %>% 
-    select(species, scientific_name) %>% 
-    distinct(species, scientific_name)
-  subsite_lm_mass = left_join(subsite_lm_mass, species_codes, by = c("species" = "species"))
-  all_lm_mass = rbind(all_lm_mass, subsite_lm_mass)
+# Create dataframe of mass and mrt time series models for each species mass over time
+model_stats = data.frame()
+for(subsite in unique(occurrences_subsites_with_temp$location)){
+  subsite_occurrences = occurrences_subsites_with_temp[occurrences_subsites_with_temp$location == subsite,]
+  min_year = min(subsite_occurrences$yr)
+  max_year = max(subsite_occurrences$yr)
+  for(species in unique(subsite_occurrences$species)){
+    species_occurrences = subsite_occurrences[subsite_occurrences$species == species,]
+    species_occurrences = complete(species_occurrences, yr = min_year:max_year)
+    mass_ts = ts(species_occurrences$mass_mean)
+    temp_ts = ts(species_occurrences$avg_temp)
+    exog_model = auto.arima(mass_ts, xreg = temp_ts)
+    exog_pvalue = coeftest(exog_model)[dim(coeftest(exog_model))[1], dim(coeftest(exog_model))[2]]
+    mass_model = Arima(mass_ts, order = arimaorder(exog_model), include.drift = TRUE)
+    mass_pvalue = coeftest(mass_model)[dim(coeftest(mass_model))[1], dim(coeftest(mass_model))[2]]
+    mass_dir = coeftest(mass_model)[dim(coeftest(mass_model))[1], 1]
+    pvalues = data.frame(subsite = subsite, species = species, exog_pvalue = exog_pvalue, 
+                         mass_pvalue = mass_pvalue, mass_dir = mass_dir)
+    model_stats = rbind(model_stats, pvalues)
+    
+  }
 }
 
+model_stats$exog_pvalue_adjust = p.adjust(model_stats$exog_pvalue)
+model_stats$mass_pvalue_adjust = p.adjust(model_stats$mass_pvalue)
+model_stats = model_stats %>% 
+  mutate(exog_pvalue_sig = case_when(exog_pvalue_adjust <= 0.05 ~ "yes", 
+                                     exog_pvalue_adjust > 0.05 ~ "no"), 
+         mass_pvalue_sig = case_when(mass_pvalue_adjust <= 0.05 ~ "yes", 
+                                     mass_pvalue_adjust > 0.05 ~ "no"))
+
 # Create dataframe of r and p values for each species temp-mass relationship
+subsites = unique(occurrences_subsites_with_temp$location)
 all_lm_mrt = data.frame(species = factor(), r.squared = numeric(), slope = numeric(), p.value = numeric(), r = numeric(), site = factor(), scientific_name = factor())
 for(each_subsite in subsites){
   subsite_annual_masses = occurrences_subsites_with_temp[occurrences_subsites_with_temp$location == each_subsite,]
@@ -111,6 +125,7 @@ for(each_subsite in subsites){
   subsite_lm_mrt = left_join(subsite_lm_mrt, species_codes, by = c("species" = "species"))
   all_lm_mrt = rbind(all_lm_mrt, subsite_lm_mrt)
 }
+all_lm_mrt = left_join(all_lm_mrt, model_stats, by = c("subsite", "species"))
 
 # Create plots for each subsite
 three_plots_df = occurrences_subsites_with_temp %>% 
@@ -136,22 +151,25 @@ three_plots_df = occurrences_subsites_with_temp %>%
 mrt_plot_df = all_lm_mrt %>% 
   group_by(subsite) %>% 
   nest() %>% 
-  mutate(r_dist = purrr::map(data, ~ggplot(., aes(x = r)) +
+  mutate(r_dist = purrr::map(data, ~ggplot(., aes(x = r, fill = exog_pvalue_sig)) +
                                geom_histogram() +
                                xlim(c(-1, 1)) +
                                geom_vline(xintercept = 0, color = "red") +
                                labs(x = "R", y = "Number of species") +
+                               scale_fill_manual(values = c("gray", "black")) +
                                theme(legend.position = "none"))
   )
 
-mass_plot_df = all_lm_mass %>% 
+mass_plot_df = model_stats %>% 
   group_by(subsite) %>% 
   nest() %>% 
-  mutate(slope_dist = purrr::map(data, ~ggplot(., aes(x = slope)) +
+  mutate(slope_dist = purrr::map(data, ~ggplot(., aes(x = mass_dir, fill = mass_pvalue_sig)) +
                                    geom_histogram() +
                                    xlim(c(-1, 1)) +
                                    geom_vline(xintercept = 0, color = "red") +
-                                   labs(x = "Yearly mass slope", y = "Number of species"))
+                                   labs(x = "Mass time series trend", y = "Number of species") +
+                                   scale_fill_manual(values = c("gray", "black")) +
+                                   theme(legend.position = "none"))
   )
 
 # Combine plots for each subsite, then combine all subsite plots
@@ -183,15 +201,13 @@ combined_sites2 = purrr::pmap(list(plots_df$combined2[1], plots_df$combined2[2],
                                                    plot_grid(..3), plot_grid(..4), 
                                                    plot_grid(..5), nrow = 1))
 
-# Save figures
-ggsave_args1 = list(filename = "plots/sev_subsites1.png", 
-                    plot = combined_sites1, 
-                    width = 18, 
-                    height = 12)
-pmap(ggsave_args1, ggsave)
+combined_sites = purrr::pmap(list(combined_sites1, combined_sites2), 
+                             ~ cowplot::plot_grid(plot_grid(..1), plot_grid(..2), 
+                                                  ncol = 1))
 
-ggsave_args2 = list(filename = "plots/sev_subsites2.png", 
-                    plot = combined_sites2, 
-                    width = 20, 
-                    height = 10)
-pmap(ggsave_args2, ggsave)
+# Save figures
+ggsave_args = list(filename = "plots/sev_subsites.png", 
+                    plot = combined_sites, 
+                    width = 18, 
+                    height = 18)
+pmap(ggsave_args, ggsave)
